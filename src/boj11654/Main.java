0ee344f23591323c7245d357c1993655eabeedf7
mgKang3646 <lordofkangs@naver.com>
2021-07-10 15:36:16 +0900

    백준 11654번 아스키코드[문자열] 문제풀이

---
+++ src/boj11654/Main.java
@@ -0,0 +1,14 @@
+package boj11654;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		int valueOfAscii = System.in.read();
+		System.out.println(valueOfAscii);
+	}
+
+}

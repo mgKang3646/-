a96ca52ef881a2dab624e9d2b5289a00e0e8ed49
mgKang3646 <lordofkangs@naver.com>
2021-07-11 09:32:54 +0900

    백준 2675번 문자열반복[문자열] 문제풀이

--- src/boj2675/Main1.java
+++ src/boj2675/Main1.java
@@ -1,10 +1,35 @@
 package boj2675;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class Main1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int testcase = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i< testcase; i++) {
+			String[] str = br.readLine().split(" ");
+			int size = Integer.parseInt(str[0]);
+			String data = str[1];
+			
+			for(int j=0; j<data.length();j++) {
+				for(int z=0; z<size;z++) {
+					bw.write(data.charAt(j));
+				}
+			}
+			bw.write("\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
 	}
-
 }
